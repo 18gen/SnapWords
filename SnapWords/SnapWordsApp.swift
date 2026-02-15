6d155cb7a5a415dd1190c864cca8df56f7acae67
18gen <ichihashigen@gmail.com>
2026-02-14 19:36:05 -0500

    Initial Commit

---
+++ SnapWords/SnapWordsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapWordsApp.swift
+//  SnapWords
+//
+//  Created by Gen Ichihashi on 2026-02-14.
+//
+
+import SwiftUI
+
+@main
+struct SnapWordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
